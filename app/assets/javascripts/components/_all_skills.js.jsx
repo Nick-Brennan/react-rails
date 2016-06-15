11d241423af52558e56cd334845b9e34811b378e
Nick Brennan <nick_p_b@yahoo.com>
2016-06-14 17:28:33 -0700

    dynamic rendering in react

--- app/assets/javascripts/components/_all_skills.js.jsx
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -1,8 +1,30 @@
 var AllSkills = React.createClass({
+
+	getInitialState: function(){
+		return { skills: [] };
+	},
+
+	componentDidMount: function(){
+		console.log("component mounted");
+		$.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
+	},
+
 	render: function(){
+
+		console.log(this.state);
+		var skills = this.state.skills.map((skill) => {
+			return (
+				<div key={skill.id}>
+					<h3>{skill.name}</h3>
+					<p><strong>Level:</strong> {skill.level}</p>
+					<p>{skill.details}</p>
+				</div>
+			)
+		});
+
 		return(
 			<div>
-				<h1>All Skills</h1>
+				{skills}
 			</div>
 		)
 	}
